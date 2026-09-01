217a8ee43a21744745bb01deda8a958a0b75aa10
Alok Singh <alok.v.singh@newgensoft.com>
2026-09-01 17:22:17 +0530

    Configure MySQL as default production database with credentials

--- backend/start-backend.ps1
+++ backend/start-backend.ps1
@@ -2,17 +2,9 @@ $ErrorActionPreference = 'Stop'
 
 $envFile = Join-Path $PSScriptRoot '..\.env'
 if (Test-Path $envFile) {
-    $envValues = @{}
     Get-Content $envFile | ForEach-Object {
         if ($_ -match '^\s*([^#=\s]+)\s*=\s*(.*?)\s*$') {
-            $envValues[$matches[1]] = $matches[2]
-        }
-    }
-
-    $useMySql = $envValues['SPRING_PROFILES_ACTIVE'] -eq 'mysql'
-    foreach ($name in $envValues.Keys) {
-        if ($useMySql -or -not $name.StartsWith('DATABASE_')) {
-            [Environment]::SetEnvironmentVariable($name, $envValues[$name], 'Process')
+            [Environment]::SetEnvironmentVariable($matches[1], $matches[2], 'Process')
         }
     }
 }
